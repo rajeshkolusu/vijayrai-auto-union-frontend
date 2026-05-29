93a04fe24ba7b32426fc25971e55748e0489f58e
rajeshkolusu <rajeshkolusu333@gmail.com>
2026-05-29 16:55:12 +0530

    connect live production api backend url

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from "axios";
 
+// This redirects ALL your frontend API calls globally to your live cloud server!
+axios.defaults.baseURL = "https://d5816076-422e-4156-8125-98bade78084f-dev.e1-us-east-azure.choreoapis.dev/default/vijayrai-auto-union-backe/v1.0/api";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
